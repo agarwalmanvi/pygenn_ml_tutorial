4bb22f5ad6e8d39c6a50a407831a7e81e40f1e1f
agarwalmanvi <94.manviagarwal@gmail.com>
2020-04-30 13:38:20 +0200

    training script for fusi rule

--- tutorial_fusi.py
+++ tutorial_fusi.py
@@ -5,6 +5,9 @@ from pygenn.genn_model import (create_custom_neuron_class,
                                create_custom_current_source_class, create_custom_weight_update_class,
                                GeNNModel, init_var, init_connectivity)
 from pygenn.genn_wrapper import NO_DELAY
+from mlxtend.data import loadlocal_mnist
+import math
+import matplotlib.pyplot as plt
 
 # ----------------------------------------------------------------------------
 # Parameters
@@ -18,11 +21,10 @@ FUSI_PARAMS = {"tauC": 60.0, "a": 0.1, "b": 0.1, "thetaV": 0.8, "thetaLUp": 3.0,
                "alpha": 0.0035, "beta": 0.0035, "Xmax": 1.0, "Xmin": 0.0, "JC": 1.0,
                "Jplus": 1.0, "Jminus": 0.0}
 TIMESTEP = 1.0
-# PRESENT_TIMESTEPS = 100
-# INPUT_CURRENT_SCALE = 1.0 / 100.0
-# OUTPUT_CURRENT_SCALE = 10.0
-# NUM_CLASSES = 10
-PRESENT_TIMESTEPS = 300
+PRESENT_TIMESTEPS = 100
+INPUT_CURRENT_SCALE = 1.0 / 100.0
+OUTPUT_CURRENT_SCALE = 10.0
+NUM_CLASSES = 10
 
 # ----------------------------------------------------------------------------
 # Custom GeNN models
@@ -79,177 +81,161 @@ fusi_model = create_custom_weight_update_class(
     is_post_spike_time_required=True
 )
 
-########## Reproduce Fig 1 from paper ################
+# Current source model which injects current with a magnitude specified by a state variable
+cs_model = create_custom_current_source_class(
+    "cs_model",
+    var_name_types=[("magnitude", "scalar")],
+    injection_code="$(injectCurrent, $(magnitude));")
 
-# Notes: done -> optimizations, sparse init with one-to-one connectivity
-#       next -> set sparse connectivity, parallel exp within models,
-#               setup data structure for recording everything.
+# ----------------------------------------------------------------------------
+# Import training data
+# ----------------------------------------------------------------------------
+data_dir = "/home/manvi/Documents/pygenn_ml_tutorial/mnist"
+X, y = loadlocal_mnist(
+    images_path=os.path.join(data_dir, 'train-images-idx3-ubyte'),
+    labels_path=os.path.join(data_dir, 'train-labels-idx1-ubyte'))
 
-pre_spike_rate = [10.0, 20.0, 30.0, 40.0, 50.0]
-post_spike_weight = [0.03, 0.04, 0.05, 0.06, 0.07, 0.08]
+X = X[:100, :]
+y = y[:100]
 
-# pre_spike_rate = [50.0]
-# post_spike_weight = [0.08]
+print("Loading training images of size: " + str(X.shape))
+print("Loading training labels of size: " + str(y.shape))
 
-for pre_rate in pre_spike_rate:
+# ----------------------------------------------------------------------------
+# Build model
+# ----------------------------------------------------------------------------
+# Create GeNN model
+model = GeNNModel("float", "tutorial_1")
+model.dT = TIMESTEP
 
-    print("\n")
-    print("Mean spike rate for presynaptic neuron: " + str(pre_rate))
+# Initial values for initialisation
+if_init = {"V": 0.0}
+fusi_init = {"X": 0.0,
+             "last_tpre": 0.0}
+fusi_post_init = {"C": 2.0}
 
-    for post_weight in post_spike_weight:
+NUM_INPUT = X.shape[1]
 
-        print("Weight for presynaptic neuron: " + str(post_weight))
+neurons_count = {"inp": NUM_INPUT,
+                 "inh": math.floor(NUM_CLASSES / 2),
+                 "out": NUM_CLASSES}
 
-        iterations = 1000
+neuron_layers = {}
 
-        for iter_num in range(iterations):
+for k in neurons_count.keys():
+    neuron_layers[k] = model.add_neuron_population(k, neurons_count[k], if_model,
+                                                   IF_PARAMS, if_init)
 
-            if iter_num % 50 == 0:
-                print("Iteration number: " + str(iter_num))
+inp2out = model.add_synapse_population(
+    "inp2out", "DENSE_INDIVIDUALG", NO_DELAY,
+    neuron_layers["inp"], neuron_layers["out"],
+    fusi_model, FUSI_PARAMS, fusi_init, {}, fusi_post_init,
+    "DeltaCurr", {}, {})
 
-            model = GeNNModel("float", "fig1")
-            model.dT = TIMESTEP
+inp2inh = model.add_synapse_population(
+    "inp2inh", "DENSE_INDIVIDUALG", NO_DELAY,
+    neuron_layers["inp"], neuron_layers["inh"],
+    "StaticPulse", {}, {"g": 1.0}, {}, {},
+    "DeltaCurr", {}, {})
 
-            presyn_params = {"rate" : pre_rate}
-            extra_poisson_params = {"rate" : 100.0}
-            poisson_init = {"timeStepToSpike" : 0.0}
-            if_init = {"V": 0.0}
-            fusi_init = {"X": 0.0,
-                         "last_tpre": 0.0}
-            fusi_post_init = {"C": 2.0}
-
-            n = 1000
-
-            presyn = model.add_neuron_population("presyn", n, "PoissonNew", presyn_params, poisson_init)
-            postsyn = model.add_neuron_population("postsyn", n, if_model, IF_PARAMS, if_init)
-            extra_poisson = model.add_neuron_population("extra_poisson", n*10, "PoissonNew",
-                                                        extra_poisson_params, poisson_init)
-
-            w = np.zeros((n, n * 10))
-            for i in range(n):
-                w[i, i * 10:(i + 1) * 10] = post_weight
-
-            pre2post = model.add_synapse_population(
-                "pre2post", "SPARSE_INDIVIDUALG", NO_DELAY,
-                presyn, postsyn,
-                fusi_model, FUSI_PARAMS, fusi_init, {}, fusi_post_init,
-                "DeltaCurr", {}, {}, init_connectivity("OneToOne", {}))
-
-            extra_poisson2post = model.add_synapse_population(
-                "extra_poisson2post", "DENSE_INDIVIDUALG", NO_DELAY,
-                extra_poisson, postsyn,
-                "StaticPulse", {}, {"g": w.flatten()}, {}, {},
-                "DeltaCurr", {}, {})
-
-            model.build()
-            model.load()
-
-            # print("Simulating")
-
-            neuron_layers = [postsyn]
-
-            # initialize arrays for storing all things we want to plot
-            layer_spikes = [(np.empty(0)) for _ in enumerate(neuron_layers)]
-            # X = np.array([fusi_init["X"]])
-            # postsyn_V = np.array([if_init["V"]])
-            # C = np.array([fusi_post_init["C"]])
-
-            # data structures to put into a csv
+inh2out = model.add_synapse_population(
+    "inh2out", "DENSE_INDIVIDUALG", NO_DELAY,
+    neuron_layers["inh"], neuron_layers["out"],
+    "StaticPulse", {}, {"g": -1.0}, {}, {},
+    "DeltaCurr", {}, {})
 
 
-            while model.timestep < PRESENT_TIMESTEPS:
-                model.step_time()
 
-                # Record spikes
-                for layer_i, l in enumerate(neuron_layers):
-                    # Download spikes
-                    model.pull_current_spikes_from_device(l.name)
+# Create current source to deliver input
+current_input = model.add_current_source("current_input", cs_model,
+                                         'inp', {}, {"magnitude": 0.0})
 
-                    # Add to data structure
-                    layer_spikes[layer_i] = (np.hstack((layer_spikes[layer_i], l.current_spikes)))
+# Create current source to deliver target output
+current_output = model.add_current_source("current_output", cs_model,
+                                          "out", {}, {"magnitude": 0.0})
 
-                # # Record value of X
-                # model.pull_var_from_device("pre2post", "X")
-                # X_val = pre2post.get_var_values("X")
-                # X = np.concatenate((X, X_val), axis=0)
-                #
-                # # Record value of postsyn_V
-                # model.pull_var_from_device("postsyn", "V")
-                # V_val = postsyn.vars["V"].view
-                # postsyn_V = np.concatenate((postsyn_V, V_val), axis=0)
-                #
-                # # Record value of C
-                # model.pull_var_from_device("pre2post", "C")
-                # C_val = pre2post.post_vars["C"].view
-                # C = np.concatenate((C, C_val), axis=0)
+# Build and load our model
+model.build()
+model.load()
 
-            unique, counts = np.unique(layer_spikes[0], return_counts=True)
-            d = dict(zip(unique, counts))
-            for k in d:
-                d[k] = d[k] / (PRESENT_TIMESTEPS / 1000)
-            model.pull_var_from_device("pre2post", "X")
-            X_val = pre2post.vars["X"].view
+# ----------------------------------------------------------------------------
+# Training
+# ----------------------------------------------------------------------------
+# Get views to efficiently access state variables
+current_input_magnitude = current_input.vars["magnitude"].view
+current_output_magnitude = current_output.vars["magnitude"].view
+layer_voltages = [l.vars["V"].view for l in list(neuron_layers.values())]
 
-            filename = str(pre_rate) + '_' + str(post_weight) + '.csv'
-            filepath = "/home/manvi/Documents/pygenn_ml_tutorial/fusi_data/LTP"
+while model.timestep < (PRESENT_TIMESTEPS * X.shape[0]):
+    # Calculate the timestep within the presentation
+    timestep_in_example = model.timestep % PRESENT_TIMESTEPS
+    example = int(model.timestep // PRESENT_TIMESTEPS)
 
-            if iter_num == 0:
-                with open(os.path.join(filepath, filename), 'w', newline="") as f:
-                    writer = csv.writer(f)
-                    writer.writerow(["post_spike_rate", "LTP_success"])
-                    for idx in d.keys():
-                        x = X_val[int(idx)]
-                        s = 1 if x > 0.5 else 0
-                        writer.writerow([d[idx], s])
-            else:
-                with open(os.path.join(filepath, filename), 'a+', newline="") as f:
-                    writer = csv.writer(f)
-                    for idx in d.keys():
-                        x = X_val[int(idx)]
-                        s = 1 if x > 0.5 else 0
-                        writer.writerow([d[idx], s])
+    # If this is the first timestep of presenting the example
+    if timestep_in_example == 0:
 
-    # # Create plot
-    # fig, axes = plt.subplots(4, sharex=True)
-    # fig.tight_layout(pad=2.0)
-    #
-    # # plot presyn spikes
-    # presyn_spike_times = layer_spikes[0][1]
-    # for s in presyn_spike_times:
-    #     axes[0].set_xlim((0,PRESENT_TIMESTEPS))
-    #     axes[0].axvline(s)
-    # axes[0].title.set_text("Presynaptic spikes")
-    #
-    # # plot X
-    # axes[1].title.set_text("Synaptic internal variable X(t)")
-    # axes[1].plot(X)
-    # axes[1].set_ylim((0,1))
-    # axes[1].axhline(0.5, linestyle="--", color="black", linewidth=0.5)
-    # axes[1].set_yticklabels(["0", "$\\theta_X$", "1"])
-    #
-    # # plot postsyn V
-    # axes[2].title.set_text('Postsynaptic voltage V(t) (Spike rate: ' + str(postsyn_spike_rate) + " Hz)")
-    # axes[2].plot(postsyn_V)
-    # axes[2].set_ylim((0,1.2))
-    # axes[2].axhline(1, linestyle="--", color="black", linewidth=0.5)
-    # axes[2].axhline(0.8, linestyle="--", color="black", linewidth=0.5)
-    # postsyn_spike_times = layer_spikes[1][1]
-    # for s in postsyn_spike_times:
-    #     axes[2].axvline(s, color="red", linewidth=0.5)
-    #
-    # # plot C
-    # axes[3].plot(C)
-    # axes[3].title.set_text("Calcium variable C(t)")
-    # for i in [3, 4, 13]:
-    #     axes[3].axhline(i, linestyle="--", color="black", linewidth=0.5)
-    #
-    # save_filename = "fig1" + str(run) + ".png"
-    # plt.savefig(save_filename)
-    # plt.close()
+        # init a data structure for plotting the raster plots for this example
+        layer_spikes = [(np.empty(0), np.empty(0)) for _ in enumerate(neuron_layers)]
 
-# print("Internal synaptic variable: ")
-# print(X)
-# print("Postsynaptic depolarization: ")
-# print(postsyn_V)
-# print("Calcium variable: ")
-# print(C)
+        if example % 10 == 0:
+            print("Example: " + str(example))
+
+        current_input_magnitude[:] = X[example, :].flatten() * INPUT_CURRENT_SCALE
+        one_hot = np.zeros((NUM_CLASSES))
+        one_hot[y[example]] = 1
+        current_output_magnitude[:] = one_hot.flatten() * OUTPUT_CURRENT_SCALE
+        model.push_var_to_device("current_input", "magnitude")
+        model.push_var_to_device("current_output", "magnitude")
+
+        # Loop through all layers and their corresponding voltage views
+        for l, v in zip(list(neuron_layers.values()), layer_voltages):
+            # Manually 'reset' voltage
+            v[:] = 0.0
+
+            # Upload
+            model.push_var_to_device(l.name, "V")
+
+    # Advance simulation
+    model.step_time()
+
+    for i, l in enumerate(list(neuron_layers.values())):
+        # Download spikes
+        model.pull_current_spikes_from_device(l.name)
+
+        # Add to data structure
+        spike_times = np.ones_like(l.current_spikes) * model.t
+        layer_spikes[i] = (np.hstack((layer_spikes[i][0], l.current_spikes)),
+                           np.hstack((layer_spikes[i][1], spike_times)))
+
+    # If this is the LAST timestep of presenting the example
+    if timestep_in_example == (PRESENT_TIMESTEPS - 1):
+
+        # Make a plot every 10th example
+        if example % 10 == 0:
+
+            print("Creating raster plot")
+
+            # Create a plot with axes for each
+            fig, axes = plt.subplots(len(neuron_layers), sharex=True)
+
+            # Loop through axes and their corresponding neuron populations
+            for a, s, l in zip(axes, layer_spikes, list(neuron_layers.values())):
+                # Plot spikes
+                a.scatter(s[1], s[0], s=1)
+
+                # Set title, axis labels
+                a.set_title(l.name)
+                a.set_ylabel("Spike number")
+                a.set_xlim((example * PRESENT_TIMESTEPS, (example + 1) * PRESENT_TIMESTEPS))
+                a.set_ylim((-1, l.size + 1))
+
+            # Add an x-axis label
+            axes[-1].set_xlabel("Time [ms]")
+            # axes[-1].hlines(testing_labels[0], xmin=0, xmax=PRESENT_TIMESTEPS,
+            #                 linestyle="--", color="gray", alpha=0.2)
+
+            # Show plot
+            save_filename = os.path.join('example' + str(example) + '.png')
+            plt.savefig(save_filename)
+
+print("Completed training.")
