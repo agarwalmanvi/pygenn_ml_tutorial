8af764e6a19502936eabe8cc0ce13997613eaea6
agarwalmanvi <94.manviagarwal@gmail.com>
2020-06-03 11:40:39 +0200

    parameter changes

--- fusi_par4.sh
+++ fusi_par4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -o ./out/outputg.%a.out
+#SBATCH -o ./out/output_class.%a.out
 #SBATCH --time=12:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=1GB
-python ./fusi_par3.py
+python ./fusi_par4.py
